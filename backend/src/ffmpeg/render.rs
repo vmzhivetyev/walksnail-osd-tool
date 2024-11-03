f1cd7a049a53ad8f20e3d3a89a50d59067688651
Yury MonZon <yurymonzon@gmail.com>
2024-11-03 22:04:57 +0100

    Higher quality video with nvenc

--- backend/src/ffmpeg/render.rs
+++ backend/src/ffmpeg/render.rs
@@ -233,7 +233,7 @@ pub fn spawn_encoder(
         }
     } else {
         if video_encoder.name.contains("nvenc") {
-            encoder_command.args(["-vf", "format=rgb24,hwupload_cuda"]);
+            encoder_command.args(["-vf", "format=rgb24,hwupload_cuda", "-preset", "p7", "-tune", "hq"]);
         }
     }
 
